900dbb6d7d1821f975904359b7523351f9fea3c5
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2022-09-28 16:11:38 -0600

    fix previous commit

--- linetension_calcs.py
+++ linetension_calcs.py
@@ -349,7 +349,7 @@ class Dislocation(StrohGeometry,metal_props):
                     return K0+K1*y+K2*y**2+K3*y**3+K4*y**4
                 y,rv2 = sp.symbols('y,rv2')
                 ysol = sp.solve(theroot(y,rv2),y) ## 4 complex roots as fcts of rv2=rho*v**2
-                yfct=sp.lambdify(rv2,ysol,modules=["scipy",{'sqrt': scipy.sqrt}])
+                yfct=sp.lambdify(rv2,ysol,modules=["scipy",{'sqrt':np.lib.scimath.sqrt}])
                 def f(x):
                     return np.abs(np.asarray(yfct(x)).imag.prod()) ## lambda=i*y, and any Re(lambda)=0 implies a divergence/limiting velocity
                 with np.errstate(invalid='ignore'):
