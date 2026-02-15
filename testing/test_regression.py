1db757c0447e653064bf06c4846ea071ad69851c
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2026-02-15 15:58:36 -0700

    port the remaining regression tests to pytest

--- testing/test_regression.py
+++ testing/test_regression.py
@@ -29,11 +29,7 @@ if Ncores>1:
     
 cwd =pathlib.Path.cwd()
 
-defaults_for_testing = {'P':0, ## pressure in strain_poly test
-                        'volpres':False, ## set to True to compute volume preserving version of the strains
-                        'fastapprox':True, ## set to False to include terms that are (close to) zero in acc_screw disloc. field
-                        'vRF_resolution':50,
-                        'vRF_fast':True}
+########## define helper functions for the tests below
 
 def readfile(fname):
     '''reads a text file (or xz compressed text file) and returns a list of its lines'''
@@ -130,14 +126,16 @@ def expand_slipsystems(metals,bccslip='all',hcpslip='all'):
             out.append(X)
     return out
 
-def test_aver(old=None,skip_calcs=False,verbose=False):
+########## tests:
+
+def test_aver(old=None,new=cwd,skip_calcs=False,verbose=False):
     '''implements regression tests for frontend script polycrystal_averaging.py'''
-    testfolder = cwd/"regressiontests"
+    testfolder = new/"regressiontests"
     testfolder.mkdir(exist_ok=True)
     if old is None:
         old = testfolder
     else:
-        old = cwd / old
+        old = new / old
     if verbose:
         print(f"{testfolder=}, baseline folder={old}")
     os.chdir(testfolder)
@@ -148,17 +146,17 @@ def test_aver(old=None,skip_calcs=False,verbose=False):
     else: print("skipping test 'aver' as requested")
     print(f"checking {fname}")
     assert diff(pathlib.Path(old,fname),pathlib.Path(testfolder,fname),verbose=verbose)
-    os.chdir(cwd)
+    os.chdir(new)
 
-def test_dragiso(old=None,skip_calcs=False,verbose=False,metals='Cu Fe',**kwargs):
+def test_dragiso(old=None,new=cwd,skip_calcs=False,verbose=False,metals='Cu Fe',**kwargs):
     '''implements regression tests for isotropic phonon drag calculations via frontend script dragcoeff_iso.py,
        where folder "old" contains the baseline results; set to "None" to initialize a new baseline.'''
-    testfolder = cwd/"regressiontests"
+    testfolder = new/"regressiontests"
     testfolder.mkdir(exist_ok=True)
     if old is None:
         old = testfolder
     else:
-        old = cwd / old
+        old = new / old
     if verbose:
         print(f"{testfolder=}, baseline folder={old}")
     os.chdir(testfolder)
@@ -185,17 +183,17 @@ def test_dragiso(old=None,skip_calcs=False,verbose=False,metals='Cu Fe',**kwargs
             assert isclose(f1,f2,verbose=verbose)
     fname = "drag_iso_fit.txt"
     assert diff(pathlib.Path(old,fname),pathlib.Path(testfolder,fname),verbose=verbose)
-    os.chdir(cwd)
+    os.chdir(new)
 
-def test_drag(old=None,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwargs):
+def test_drag(old=None,new=cwd,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwargs):
     '''implements regression tests for anisotropic phonon drag calculations via frontend script dragcoeff_semi_iso.py,
        where folder "old" contains the baseline results; set to "None" to initialize a new baseline.'''
-    testfolder = cwd/"regressiontests"
+    testfolder = new/"regressiontests"
     testfolder.mkdir(exist_ok=True)
     if old is None:
         old = testfolder
     else:
-        old = cwd / old
+        old = new / old
     if verbose:
         print(f"{testfolder=}, baseline folder={old}")
     os.chdir(testfolder)
@@ -229,18 +227,18 @@ def test_drag(old=None,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwa
             assert isclose(f1,f2,verbose=verbose)
     fname = "drag_semi_iso_fit.txt"
     assert diff(pathlib.Path(old,drag_folder,fname),pathlib.Path(testfolder,drag_folder,fname),verbose=verbose)
-    os.chdir(cwd)
+    os.chdir(new)
     
 
-def test_LT(old=None,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwargs):
+def test_LT(old=None,new=cwd,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwargs):
     '''implements regression tests for line tension calculations via frontend script linetension_calcs.py,
        where folder "old" contains the baseline results; set to "None" to initialize a new baseline.'''
-    testfolder = cwd/"regressiontests"
+    testfolder = new/"regressiontests"
     testfolder.mkdir(exist_ok=True)
     if old is None:
         old = testfolder
     else:
-        old = cwd / old
+        old = new / old
     if verbose:
         print(f"{testfolder=}, baseline folder={old}")
     os.chdir(testfolder)
@@ -268,4 +266,187 @@ def test_LT(old=None,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwarg
             f2 = read_2dresults(pathlib.Path(testfolder,folder,f"LT_{X}.dat.xz"))
             assert isclose(f1,f2,verbose=verbose)
         assert diff(pathlib.Path(old,folder,fname),pathlib.Path(testfolder,folder,fname),verbose=verbose) is True
-    os.chdir(cwd)
+    os.chdir(new)
+
+def test_acc(old=None,new=cwd,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwargs):
+    '''implements regression tests for accelerating dislocation solutions,
+       where folder "old" contains the baseline results; set to "None" to initialize a new baseline.'''
+    testfolder = new/"regressiontests"
+    testfolder.mkdir(exist_ok=True)
+    if old is None:
+        old = testfolder
+    else:
+        old = new / old
+    if verbose:
+        print(f"{testfolder=}, baseline folder={old}")
+    os.chdir(testfolder)
+    opts = {'bccslip':'all','hcpslip':'all','fastapprox':True} # defaults for this test
+    opts['Ncores'] = Ncores
+    opts.update(kwargs)
+    metal_acc_screw = []
+    metal_acc_edge = []
+    metal_list = expand_slipsystems(metals,bccslip=opts['bccslip'],hcpslip=opts['hcpslip'])
+    for X in metal_list:
+        if X in fcc_metals or "basal" in X or "prismatic" in X or "pyramidal" in X or X in tetr_metals:
+            metal_acc_screw.append(X)
+        if "112" in X or "basal" in X or "prismatic" in X or X in tetr_metals:
+            metal_acc_edge.append(X)
+    if not skip_calcs:
+        print("running test 'acc' ...")
+        tmppydislocdyn = pathlib.Path("temp_pydislocdyn")
+        tmppydislocdyn.mkdir(exist_ok=True)
+        os.chdir(tmppydislocdyn)
+        writeallinputfiles()
+        os.chdir("..")
+        uij_acc_screw = {}
+        acc_screw = {}
+        uij_acc_edge = {}
+        acc_edge = {}
+        print(f"calculating accelerating screw dislocation fields for {metal_acc_screw}")
+        for X in metal_acc_screw:
+            acc_screw[X] = readinputfile(pathlib.Path("temp_pydislocdyn",X),Nphi=50)
+            acc_screw[X].computevcrit()
+            vel=0.9*acc_screw[X].vcrit_screw
+            if verbose:
+                print(f"{X}: testing for constant acceleration")
+            acc_screw[X].plotdisloc(vel/acc_screw[X].ct,a=1e14,fastapprox=opts['fastapprox'])
+            uij_acc_screw[X] = pd.DataFrame(acc_screw[X].uij_acc_screw_aligned[2,0],index=acc_screw[X].r,columns=acc_screw[X].phi/np.pi)
+            uij_acc_screw[X].index.name="r[burgers]"
+            uij_acc_screw[X].columns.name="phi[pi]"
+            uij_acc_screw[X].to_csv(pathlib.Path(testfolder,f"uij_acc_screw_{X}.csv.xz"),compression='xz')
+            ## another dynamic solution:
+            ## assume l(t) = adot*t**3/6 ## (i.e. acceleration starts at 0 and increases at rate adot from t>0)
+            adot = 6.2e25 ## time-derivative of acceleration, acc is initially zero at time t=0
+            def eta(x):
+                '''returns time as a function of position x for the special case of a constant acceleration rate adot.'''
+                return np.sign(x)*np.cbrt(6*abs(x)/adot)
+            def etapr(x):
+                '''returns the derivative of eta(x) (units: one over velocity)'''
+                return eta(x)/(3*x)
+            time = np.sqrt(2*vel/adot) ## vel=adot*t**2/2, time=t(vel)
+            distance = adot*time**3/6 ## distance covered by the core at time 'time'
+            acc = adot*time ## current acceleration at time 'time'
+            if verbose:
+                print("testing fully dynamic solution: assume acceleration starts at 0 and increases at rate a-dot from t>0.")
+                print(f"time to reach {vel=:.2f}m/s with a-dot={adot:.2e}m/s^3: t(v)={time:.2e}s")
+                print(f"current acceleration at time t(v) is: a(t(v))={acc:.2e}m/s^2")
+                print(f"dislocation moved distance d={distance:.2e}m when it reached velocity v\n")
+            acc_screw[X].plotdisloc(a=None,eta_kw=eta,etapr_kw=etapr,t=time,shift=distance,beta=vel/acc_screw[X].ct,fastapprox=opts['fastapprox'])
+            uij_acc_screw[X] = pd.DataFrame(acc_screw[X].uij_acc_screw_aligned[2,0],index=acc_screw[X].r,columns=acc_screw[X].phi/np.pi)
+            uij_acc_screw[X].index.name="r[burgers]"
+            uij_acc_screw[X].columns.name="phi[pi]"
+            uij_acc_screw[X].to_csv(pathlib.Path(testfolder,f"uij_acc_screw_alt_{X}.csv.xz"),compression='xz')
+        print(f"calculating accelerating edge dislocation fields for {metal_acc_edge}")
+        for X in metal_acc_edge:
+            acc_edge[X] = readinputfile(pathlib.Path("temp_pydislocdyn",X),Nphi=25)
+            acc_edge[X].computevcrit()
+            acc_edge[X].plotdisloc(0.9*acc_edge[X].vcrit_edge/acc_edge[X].ct,a=1e14,character='edge',component=[1,1],Nr=25)
+            uij_acc_edge[X] = pd.DataFrame(acc_edge[X].uij_acc_edge_aligned[1,1],index=acc_edge[X].r,columns=acc_edge[X].phi/np.pi)
+            uij_acc_edge[X].index.name="r[burgers]"
+            uij_acc_edge[X].columns.name="phi[pi]"
+            uij_acc_edge[X].to_csv(pathlib.Path(testfolder,f"uij_acc_edge_{X}.csv.xz"),compression='xz')
+    else: print("skipping test 'acc' as requested")
+    print("\ncomparing acc results")
+    for X in metal_acc_screw:
+        for fname in ("uij_acc_screw_", "uij_acc_screw_alt_"):
+            fending = ".csv.xz"
+            if not pathlib.Path(old,f"{fname}{X}{fending}").is_file():
+                fending = ".csv" # support reading old uncompressed files
+            f1 = pd.read_csv(pathlib.Path(old,f"{fname}{X}{fending}"),index_col=0)
+            f2 = pd.read_csv(pathlib.Path(testfolder,f"{fname}{X}.csv.xz"),index_col=0)
+            assert isclose(f1,f2,verbose=verbose)
+    for X in metal_acc_edge:
+        f1 = pd.read_csv(pathlib.Path(old,f"uij_acc_edge_{X}{fending}"),index_col=0)
+        f2 = pd.read_csv(pathlib.Path(testfolder,f"uij_acc_edge_{X}.csv.xz"),index_col=0)
+        assert isclose(f1,f2,verbose=verbose)
+    os.chdir(new)
+
+def test_misc(old=None,new=cwd,skip_calcs=False,verbose=False,metals='Al Mo Ti Sn',**kwargs):
+    '''implements various regression tests for the dislocation class,
+       where folder "old" contains the baseline results; set to "None" to initialize a new baseline.'''
+    testfolder = new/"regressiontests"
+    testfolder.mkdir(exist_ok=True)
+    if old is None:
+        old = testfolder
+    else:
+        old = new / old
+    if verbose:
+        print(f"{testfolder=}, baseline folder={old}")
+    os.chdir(testfolder)
+    opts = {'bccslip':'all','hcpslip':'all','fastapprox':True,'vRF_resolution':50,'vRF_fast':True,'P':0,'volpres':False} # defaults for this test
+    opts['Ncores'] = Ncores
+    opts.update(kwargs)
+    crystalsyms = ['iso', 'cubic', 'hcp', 'tetr', 'trig', 'tetr2', 'orth', 'mono', 'tric']
+    metal_list = expand_slipsystems(metals,bccslip=opts['bccslip'],hcpslip=opts['hcpslip'])
+    if not skip_calcs:
+        print("running test 'misc' ...")
+        tmppydislocdyn = pathlib.Path("temp_pydislocdyn")
+        tmppydislocdyn.mkdir(exist_ok=True)
+        os.chdir(tmppydislocdyn)
+        writeallinputfiles()
+        os.chdir("..")
+        Y = {}
+        print(f"calculating limiting velocities, Rayleigh speeds, and radiation-free velocities for {metal_list}")
+        for X in metal_list:
+            Y[X] = readinputfile(pathlib.Path("temp_pydislocdyn",X),Ntheta=5)
+            Y[X].computevcrit()
+            Y[X].findvcrit_smallest()
+            Y[X].findRayleigh()
+            Y[X].find_vRF(fast=opts['vRF_fast'],resolution=opts['vRF_resolution'])
+            with open(X+"props.txt", "w", encoding="utf8") as logfile:
+                np.set_printoptions(precision=2,suppress=True)
+                logfile.write(repr(Y[X]))
+                logfile.write("\n\ntheta:\n")
+                logfile.write('\n'.join(map("{:.6f}".format,Y[X].theta)))
+                logfile.write(f'\nvcrit(theta)={Y[X].vcrit_all[1]}')
+                logfile.write(f'\nvcrit_smallest={Y[X].vcrit_smallest:.2f}')
+                logfile.write(f'\nvRayleigh(theta)={Y[X].Rayleigh}')
+                logfile.write(f'\nvRF={round_list(Y[X].vRF)}')
+        print("calculating various dislocation fields")
+        for X in metal_list:
+            Y[X].plotdisloc(0.5,nogradient=True,component=2,savefig=False)
+            Y[X].computeuij(0.5,r=Y[X].r)
+            Y[X].alignuij()
+            np.savez_compressed(f"u_{X}.npz",uk_05small=Y[X].uk_aligned[:,:,::10,::10],uij_05small=Y[X].uij_aligned[:,:,:,::10,::10])
+        print("calculating various deformations within the 'strain_poly' class")
+        y = sp.Symbol('y')
+        eta1, eta2, eta3, eta4, eta5, eta6 = sp.symbols('eta1 eta2 eta3 eta4 eta5 eta6')
+        etaS = np.array([eta1, eta2, eta3, eta4, eta5, eta6])
+        def maincomputations(sym):
+            poly=strain_poly(y=y,sym=sym)
+            phi = poly.generate_poly(etaS,make_eta=False,P=opts['P'])
+            alpha = {}
+            polynom = {}
+            strain = [[y,0,0,0,0,0],[y,y,y,0,0,0],[y,0,0,y,0,0],[0,0,0,y,y,y],[0,y,-y,y,y,0],[y,0,0,y,y,y],[0,0,0,y,0,0],[y,-y,0,0,0,y]]
+            for i,strni in enumerate(strain):
+                polynom[i] = poly.generate_poly(strni,preserve_volume=opts['volpres'],P=opts['P'])
+                alpha[i] = poly.alpha
+                strain[i] = Voigt(poly.strain)
+            with open(f"deformations_results_{sym}.txt","w", encoding="utf8") as deffile:
+                deffile.write(f"{phi=}\n")
+                deffile.write("\nbelow we list alphas and corresponding phi2+phi3 polynomials; alphas are in Voigt notation (only consider symmetric cases)\n")
+                for i in range(len(strain)):
+                    deffile.write(f"\nalpha[{i}]: {np.array2string(Voigt(alpha[i]), separator=', ')}\n")
+                    deffile.write(f"poly[{i}]: {polynom[i]}\n")
+        if Ncores>1:
+            Parallel(n_jobs=Ncores)(delayed(maincomputations)(sym) for sym in crystalsyms)
+        else:
+            [maincomputations(sym) for sym in crystalsyms]
+    else: print("skipping tests 'misc' as requested")
+    print("\ncomparing misc results")
+    for X in metal_list:
+        fname = X+"props.txt"
+        assert diff(pathlib.Path(old,fname),pathlib.Path(testfolder,fname),verbose=verbose)
+        fname = f"u_{X}.npz"
+        if pathlib.Path(old,fname).is_file():
+            u_results1 = np.load(pathlib.Path(old,fname))
+        else:
+            u_results1 = {'uk_05small':np.load(pathlib.Path(old,f'uk_05small_{X}.npy')),'uij_05small':np.load(pathlib.Path(old,f'uij_05small_{X}.npy'))}
+        u_results2 = np.load(pathlib.Path(testfolder,fname))
+        for aname in ['uk_05small','uij_05small']:
+            f1 = u_results1[aname]
+            f2 = u_results2[aname]
+            assert isclose(f1,f2)
+    for sym in crystalsyms:
+        fname = f"deformations_results_{sym}.txt"
+        assert diff(pathlib.Path(old,fname),pathlib.Path(testfolder,fname),verbose=verbose)
